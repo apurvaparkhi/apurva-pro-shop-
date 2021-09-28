ecb37ce0bcb5d7ce012346dcd6c41432cca67f72
apurvaparkhi <aparkhi78@gmail.com>
2021-09-28 12:51:54 +0530

    Rating Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,24 @@
 import {Navbar, Nav, Container} from 'react-bootstrap'
-
+import{ LinkContainer } from 'react-router-bootstrap'
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark"  variant='dark' expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="#home">Proshop</Navbar.Brand>
+                <LinkContainer to='/'>
+                        <Navbar.Brand >Proshop</Navbar.Brand>
+                </LinkContainer>
+                
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'> Cart</i></Nav.Link>
-                        <Nav.Link href="/login"><i className='fas fa-user'> SignIn</i></Nav.Link>
+                        <LinkContainer to='/cart'>
+                            <Nav.Link >
+                                <i className='fas fa-shopping-cart'> Cart</i></Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className='fas fa-user'> SignIn</i></Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
